b21bf3406c55bbc7a2bd250824e95bed169cb734
Julia Aasmaa <my_e_10@hotmail.com>
2021-09-07 11:45:14 +0300

    If Statements

---
+++ IfStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust
+            //kui kasutaja on vanem, kui 18, siis konsoolis kuvatakse "oled piisavalt vana"
+            //kui kasutaja on noorem, kui 18, siis konsoolis kuvatakse "oled liiga noor"
+            //kui kasutaja on 18, siis konsoolis kuvatakse "oledki 18"
+
+            Console.WriteLine("Kui vana sa oled?");
+
+            int Age = Int32.Parse(Console.ReadLine());
+
+            if (Age > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana");
+            }            
+            else if (Age < 18)
+            {
+                Console.WriteLine("Oled liiga noor");
+            }
+            else
+            {
+                Console.WriteLine("Oledki 18.");
+            }
+
+            Console.WriteLine("Ilusat päeva!");
+
+        }
+    }
+}
---
+++ PositiveNegative/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
